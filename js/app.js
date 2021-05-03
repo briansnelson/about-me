66ede701c947b84c262109334c73349fc7d23377
briansnelson <brian@grit360.com>
2021-05-02 17:25:12 -0700

    added last counter

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 //console.log('Hello World!');
-
+let totalScore = 0
 let username = prompt('Hello! What is your name?');
 
 alert('What\'s up, ' + username + '? Let\'s get to know each other. Please answer yes or no y/n to the questions.');
@@ -11,6 +11,8 @@ let questionOne = prompt('Was I a Soldier? Please answer yes or no y/n.').toLowe
 if(questionOne === 'yes' || questionOne === 'y'){
   //console.log('questionOne:' + questionOne);
   alert('That\'s right!');
+  totalScore++;
+
 } else if(questionOne === 'no' || questionOne === 'n'){
   alert('No, in fact I was a Soldier for 27 years.');
 }
@@ -22,6 +24,7 @@ if(questionTwo === 'no'  || questionTwo === 'n'){
   alert('I\'m basically unemployable, ' + username + ' so I can only work for myself!');
 } else if(questionTwo === 'yes' || questionTwo === 'y'){
   alert('You Sir aaaarrrrrreee correct!');
+  totalScore++;
 }
 
 let questionThree = prompt('Am I married? Please answer yes or no y/n.').toLowerCase();
@@ -32,6 +35,7 @@ if(questionThree === 'no' || questionThree === 'n'){
   
 } else if(questionThree === 'yes' || questionThree === 'y'){
   alert('Yes! 29 years! I\'m a lucky man!');
+  totalScore++;
 }
 
 let questionFour = prompt('Do I rescue animals? Please answer yes or no.').toLowerCase();
@@ -41,12 +45,15 @@ if(questionFour === 'no' || questionFour === 'n'){
   alert('Actually my wife and I co-founded Kindred Souls Foundation in 2005.');
 } else if(questionFour === 'yes' || questionFour === 'y'){
   alert('Yes! Hope to see you run the stairs this August!');
+  totalScore++;
+
 }
 
 
 let questionFive = prompt('Do I help Veterans find jobs? Please answer yes or no y/n.').toLowerCase();
 //console.log(questionFive)
 alert(username + ' I do help Veterans find jobs. Thanks for taking the time to get to know me. Have a great day.');
+totalScore++;
 
 let correctAnswer = 51
 let tries = 4
@@ -58,6 +65,7 @@ for(let i = 0; i < tries; i++){
   let questionSix = +prompt('How old am I? Take a stab, you got 4 chances to be right.');
   if(questionSix === correctAnswer){
     alert(`Well done!, I am ${correctAnswer} years old.`);
+    totalScore++;
     break;
 //console.log('Well done!, I am 51 years old.')
   } else if(questionSix < correctAnswer){
@@ -106,7 +114,8 @@ while (attemptsRemaining && !answer){
   for (let i = 0; i < formerTitles.length; i++){
     if(questionSeven === formerTitles[i]){
     alert(`Correct. I've also been called ${formerTitles}`);
-    answer=true
+    answer=true;
+    totalScore++;
     break;
       //console.log(questionSeven);
     }
@@ -116,6 +125,10 @@ while (attemptsRemaining && !answer){
   }
   //console.log(`Not exactly, try again = ${attemptsRemaining}`);
 }
+console.log(totalScore);
+
+alert(`Congrats Bro! You got ${totalScore} of 7 question.`);
+
 
 
 
