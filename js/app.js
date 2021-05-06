792a493ae7803ee9b05d8596dfb0cd0db7634108
briansnelson <brian@grit360.com>
2021-05-05 19:54:22 -0700

    made questions into arrays

--- js/app.js
+++ js/app.js
@@ -3,15 +3,18 @@
 //declare variables
 let totalScore = 0;
 let username = prompt('Hello! What is your name?');
+let questionArray = ['Was I a Soldier?', 'Do I own a business?', 'Am I married?', 'Do I rescue animals?', 'Do I help Veterans find jobs?', 'How old am I?', 'How have people addressed me over the years?'];
 
+let corrrectAnswers = ['yes', 'y', 51, 'Sir', 'Major', 'Sergeant', 'Private', 'Veteran', 'Dirty Hippie'];
 alert('What\'s up, ' + username + '? Let\'s get to know each other. Please answer yes or no y/n to the questions.');
+
 
 //functions declared
 function q1(){
-  let questionOne = prompt('Was I a Soldier? Please answer yes or no y/n.').toLowerCase();
+  let questionOne = prompt(`${questionArray[0]} Please answer yes or no y/n.`).toLowerCase();
   //console.log(questionOne);
 
-  if(questionOne === 'yes' || questionOne === 'y'){
+  if(questionOne === corrrectAnswers[0] || questionOne === corrrectAnswers[1]){
     //console.log('questionOne:' + questionOne);
     alert('That\'s right!');
     totalScore++;
@@ -21,41 +24,41 @@ function q1(){
   }
 }
 function q2(){
-  let questionTwo = prompt('Do I own a business? Please answer yes or no y/n.').toLowerCase();
+  let questionTwo = prompt(`${questionArray[1]} Please answer yes or no y/n.`).toLowerCase();
   //console.log(questionTwo);
   if(questionTwo === 'no'  || questionTwo === 'n'){
     alert('I\'m basically unemployable, ' + username + ' so I can only work for myself!');
-  } else if(questionTwo === 'yes' || questionTwo === 'y'){
+  } else if(questionTwo === corrrectAnswers[0] || questionTwo === corrrectAnswers[1]) {
     alert('You Sir aaaarrrrrreee correct!');
     totalScore++;
   }
 }
 function q3(){
-  let questionThree = prompt('Am I married? Please answer yes or no y/n.').toLowerCase();
+  let questionThree = prompt(`${questionArray[2]}Please answer yes or no y/n.`).toLowerCase();
   //console.log(questionThree);
 
   if(questionThree === 'no' || questionThree === 'n'){
     alert('Incorrect.' + username + '!' + ' Back of the line.');
     
-  } else if(questionThree === 'yes' || questionThree === 'y'){
+  } else if(questionThree === corrrectAnswers[0] || questionThree === corrrectAnswers[1]){
     alert('Yes! 29 years! I\'m a lucky man!');
     totalScore++;
   }
 }
 function q4(){
-  let questionFour = prompt('Do I rescue animals? Please answer yes or no.').toLowerCase();
+  let questionFour = prompt(`${questionArray[3]} Please answer yes or no.`).toLowerCase();
   //console.log(questionFour);
 
   if(questionFour === 'no' || questionFour === 'n'){
     alert('Actually my wife and I co-founded Kindred Souls Foundation in 2005.');
-  } else if(questionFour === 'yes' || questionFour === 'y'){
+  } else if(questionFour === corrrectAnswers[0] || questionFour === corrrectAnswers[1]){
     alert('Yes! Hope to see you run the stairs this August!');
     totalScore++;
 
   }
 }
 function q5(){
-  let questionFive = prompt('Do I help Veterans find jobs? Please answer yes or no y/n.').toLowerCase();
+  let questionFive = prompt(`${questionArray[4]} Please answer yes or no y/n.`).toLowerCase();
   //console.log(questionFive)
   alert(username + ' I do help Veterans find jobs. Thanks for taking the time to get to know me. Have a great day.');
   totalScore++;
@@ -68,8 +71,8 @@ function q6(){
   //console.log(questionSix);
 
   for(let i = 0; i < tries; i++){
-    let questionSix = +prompt('How old am I? Take a stab, you got 4 chances to be right.');
-    if(questionSix === correctAnswer){
+    let questionSix = +prompt(`${questionArray[5]} Take a stab, you got 4 chances to be right.`);
+    if(questionSix === corrrectAnswers[2]){
       alert(`Well done!, I am ${correctAnswer} years old.`);
       totalScore++;
       break;
@@ -114,10 +117,10 @@ function q7(){
   while (attemptsRemaining && !answer){
 
     attemptsRemaining--;
-    let questionSeven = prompt('How have people addressed me over the years?');
+    let questionSeven = prompt(`${questionArray[6]}`);
     //console.log('How have people addressed me over the years?');
-    for (let i = 0; i < formerTitles.length; i++){
-      if(questionSeven === formerTitles[i]){
+    for (let i = 3; i < corrrectAnswers.length; i++){
+      if(questionSeven === corrrectAnswers[i]){
         alert(`Correct. I've also been called ${formerTitles}`);
         answer=true;
         totalScore++;
